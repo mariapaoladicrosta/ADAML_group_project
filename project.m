719044a00ca9b761434b4729b6a96189f6a3020f
mariapaoladicrosta <131494033+mariapaoladicrosta@users.noreply.github.com>
2024-09-14 18:03:50 +0300

    Add files via upload

---
+++ project.m
@@ -0,0 +1,26 @@
+clear all, close all, clc
+
+% loading the file
+data = readtable('/Users/mariapaoladicrosta/Downloads/MiningProcess_Flotation_Plant_Database.csv');
+
+%%
+% exploring data
+whos data
+
+size(data)
+
+head(data)
+
+summary(data)
+
+%%
+% transform cell array of character vectors into double arrays 
+for i = 1:size(data,2)
+    if iscell(data{:,i})  % Check if the column is a cell array
+        data.(data.Properties.VariableNames{i}) = str2double(data{:,i});  % Convert cell array of character vectors to numeric
+    end
+end
+
+summary(data)
+
+%%
